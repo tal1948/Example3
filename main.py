95a4211fb4e1a44dadb9bb57a6a7d39d3248eadf
Tal Barak <tal1948@outlook.com>
2024-12-29 21:40:45 +0200

    Added the sub_numbers function

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ def add_numbers(a, b, c):
     """This function returns the same of 3 numbers"""
     return a + b + c
 
+def sub_numbers(a, b, c):
+    return a - b - c
+
 def print_numbers(a, b, c):
     print (f"a:{a}, b:{b}, c:{c}")
